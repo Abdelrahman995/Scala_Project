461fd40f4ee57e6112d9f6d267aab80925d92de2
Abdelrahman995 <44041002+Abdelrahman995@users.noreply.github.com>
2020-04-26 14:06:25 +0200

    Add files via upload

--- src/test/scala/MyMain.scala
+++ src/test/scala/MyMain.scala
@@ -1,17 +1,7 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark
-import org.apache.spark.sql.{SQLContext, SaveMode}
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.expressions.Window
-
-import scala.io.Source
-import java.io.File
-
-import org.apache.hadoop.fs.FileSystem
+import org.apache.spark.sql.{AnalysisException, Row}
 import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType}
 
-import scala.reflect.io.File
-import scala.collection.mutable.ListBuffer
 
 object MyMain {
 
@@ -24,7 +14,7 @@ object MyMain {
     System.setProperty("hadoop.home.dir", "C:\\hadoop")
     val conf = new SparkConf()
     conf.setMaster("local") //local machine
-    conf.setAppName(" Scala Final Project ")
+    conf.setAppName(" Scala Project ")
     val sc = new SparkContext(conf)
     val sqlContext= new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
@@ -38,80 +28,83 @@ object MyMain {
         StructField("TrafficVol", IntegerType, true),
         StructField("Time", LongType, true)))
 
-    val Data = sqlContext.read.option("header", "false").schema(Data_Schema).csv("D:\\ITI\\Scala\\Data")
-    println(" ===== ALL DATA FILES  =====")
-    Data.write.mode("append").csv("D:\\ITI\\Scala\\data_Generated")
+    if (args.length > 3) {
+      val data_path: String = args(0)
+      val rules_path: String = args(1)
+      val segments_path: String = args(2)
 
-    //Reading rules Csv file
-    val rdd_rules = sc.textFile("D:\\ITI\\Scala\\RULES.csv")
-    println("File RULES.csv Read Successfully")
-    val df_rules = rdd_rules.map {
-      line =>
-        val column = line.split(",")
-        rule(column(0), column(1), column(2).toInt, column(3).toInt,column(4).toInt)
-    }.toDF()
-    //df_rules.show()
+      var Data = sqlContext.createDataFrame(sc.emptyRDD[Row], Data_Schema)
+      try {
+        Data = sqlContext.read.option("header", "false").schema(Data_Schema).csv(data_path)
+      } catch {
+        case ex: AnalysisException => {
+          println("Error: Path does not exist!")
+          System.exit(1)
+        }
+        case unknown: Exception => {
+          println(s"Unknown exception: $unknown")
+          System.exit(1)
+        }
+      }
 
-    //Reading Segment Csv file
-    val rdd_segment = sc.textFile("D:\\ITI\\Scala\\SEGMENT.csv")
-    println("File SEGMENT.csv Read Successfully")
-    val df_segment = rdd_segment.map {
-      line =>
-        val column = line.split(",")
-        segment(column(0))
-    }.toDF()
-    //df_segment.show()
+      //Reading rules Csv file
+      var df_rules = sqlContext.createDataFrame(sc.emptyRDD[Row], Data_Schema)
+      try {
+        val rdd_rules = sc.textFile(rules_path)
+        println("File RULES.csv Read Successfully")
+        df_rules = rdd_rules.map {
+          line =>
+            val column = line.split(",")
+            rule(column(0), column(1), column(2).toInt, column(3).toInt, column(4).toInt)
+        }.toDF()
+      } catch {
+        case _: Throwable => {
+          println("Error: Input path does not exist!")
+          System.exit(1)
+        }
+        case unknown: Exception => {
+          println(s"Unknown exception: $unknown")
+          System.exit(1)
+        }
+      }
 
+      //Reading Segment Csv file
+      var df_segment = sqlContext.createDataFrame(sc.emptyRDD[Row], Data_Schema)
+      try {
+        val rdd_segment = sc.textFile(segments_path)
+        println("File SEGMENT.csv Read Successfully")
+        df_segment = rdd_segment.map {
+          line =>
+            val column = line.split(",")
+            segment(column(0))
+        }.toDF()
+      } catch {
+        case _: Throwable => {
+          println("Error: Input path does not exist!")
+          System.exit(1)
+        }
+        case unknown: Exception => {
+          println(s"Unknown exception: $unknown")
+          System.exit(1)
+        }
+      }
 
-      // Filteration of Data on Segment numbers
+      // Join segments with data
       val df_data_segment = Data.join(df_segment, "MSISDN")
-      //println(" Data and Segment joined data frame output :- ")
-      //df_data_segment.show()
-      //println(" COUNT OF DATA JOINED WITH SEGMENT :- "+df_data_segment.count())
 
-      println(" data frame output :- ")
+      // Join (segments and data) with rules, where TrafficVol greater than TotalVol
       val df_output = df_data_segment.join(df_rules, "ServiceId").filter($"TrafficVol" > $"TotalVol").coalesce(1)
-      // println(" COUNT OF DATA JOINED WITH SEGMENT After Filter  :- "+df_output.count())
 
-      val grouped_df = df_output.groupBy("MSISDN","ServiceId").min("TrafficVol","Time")
-      println(" COUNT OF DATA JOINED WITH SEGMENT After Filter  :- "+grouped_df.count())
-      grouped_df.show()
+      // Filter data with older timestamp for the segments and services
+      val grouped_df = df_output.groupBy("MSISDN", "ServiceId").min("TrafficVol", "Time")
 
-      //val final_output =  grouped_df.join(df_output, grouped_df("ServiceId") === df_output("ServiceId") && grouped_df("MSISDN") === df_output("MSISDN"),"left")
-      //println(Data_Files)
-      //var x = a.concat("OUTPUT")
-    grouped_df.write.mode("append").csv("D:\\ITI\\Scala\\Success")
+      // Write the output
+      grouped_df.write.mode("append").csv("C:\\ITI_Sala_Project\\Output")
+      println("Data frame csv file saved successfully!")
 
-      println(" data frame csv file saved successfully .. ")
+    } else {
+      println("Error: Some arguments are missing!")
+      System.exit(1)
+    }
   }
 }
-
-
-/*
-    val df_data = rdd_data.map(line => line.split(",")).map(fields => (fields(1) , (fields(0),fields(2),fields(3))))
-    /*println("Pair RDD data")
-    pair_data.foreach(f=>{
-      println(f)
-    })*/
-
-    val pair_rules = rdd_rules.map(line => line.split(",")).map(fields => (fields(0) , (fields(1),fields(2),fields(3),fields(4))))
-    /*println("Pair RDD Rules")
-    pair_rules.foreach(f=>{
-      println(f)
-    })*/
-
-    val dataDF = rdd_data.map {
-      line =>
-        val column = line.split(",")
-        data(column(0), column(1), column(2).toInt, column(3).toDouble)
-    }.toDF()
-
-    dataDF.show()
-
-    /*val joined_rdd = pair_data.join(pair_rules)
-    println("============  JOIN RDDS  ============")
-    joined_rdd.foreach(f=>{
-      println(f)
-    })*/
- */
-
